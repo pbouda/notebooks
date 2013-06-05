ebb48fe2296cb1d9b8b5fb41a168789efa521e03
Peter Bouda <pbouda@cidles.eu>
2013-06-05 11:39:42 +0100

    Added notebook for Diana Hinuq analysis.

---
+++ helpers/diana.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import csv
+import codecs
+
+import poioapi.io.graf
+import poioapi.annotationgraph
+import poioapi.data
+
+def from_excel(filepath):
+    ag = poioapi.annotationgraph.AnnotationGraph()
+    parser = ExcelParser(filepath)
+    converter = poioapi.io.graf.GrAFConverter(parser)
+    converter.parse()
+    ag.tier_hierarchies = converter.tier_hierarchies
+    ag.structure_type_handler = poioapi.data.DataStructureType(ag.tier_hierarchies[0])
+    ag.graf = converter.graf
+    return ag
+
+class ExcelParser(poioapi.io.graf.BaseParser):
+
+    def __init__(self, filepath):
+        self.word_orders = dict()
+        self.clauses = list()
+        self.clause_types = dict()
+        self.last_id = -1
+        with codecs.open(filepath, "r", "utf-8") as csvfile:
+            hinuq2 = csv.reader(csvfile, delimiter='|')
+            i = 0
+            for row in hinuq2:
+                if i == 2:
+                    clause_ids = row
+                elif i == 3:
+                    clause_types = row
+                elif i == 4:
+                    grammatical_relations = row
+                i += 1  
+                if i > 7:
+                    # now parse
+                    word_order = []
+                    c_id = None
+                    prev_c_id = None
+                    for j, clause_id in enumerate(clause_ids):
+
+                        # new clause
+                        if clause_id != "":
+                            # add word order to previous clause
+                            if len(word_order) > 0:
+                                self.word_orders[c_id] = word_order
+                            word_order = []
+                            
+                            # add new clause
+                            c_id = self._next_id()
+                            self.clauses.append(c_id)
+                            self.clause_types[c_id] = clause_types[j].strip()
+                        
+                        grammatical_relation = grammatical_relations[j].strip()
+                        word_order.append(grammatical_relation)
+
+                    if len(word_order) > 0:
+                        self.word_orders[c_id] = word_order
+                    i = 0
+
+    def _next_id(self):
+        self.last_id += 1
+        return self.last_id
+
+    def get_root_tiers(self):
+        return [poioapi.io.graf.Tier("clause_id")]
+
+    def get_child_tiers_for_tier(self, tier):
+        if tier.name == "clause_id":
+            return [poioapi.io.graf.Tier("grammatical_relation"),
+                    poioapi.io.graf.Tier("clause_type")]
+
+        return None
+
+    def get_annotations_for_tier(self, tier, annotation_parent=None):
+        if tier.name == "clause_id":
+            return [poioapi.io.graf.Annotation(i, v) for i, v in enumerate(self.clauses)]
+
+        elif tier.name == "clause_type":
+            return [poioapi.io.graf.Annotation(self._next_id(), self.clause_types[annotation_parent.id])]
+
+        elif tier.name == "grammatical_relation":
+            return [poioapi.io.graf.Annotation(self._next_id(), v)  for v in self.word_orders[annotation_parent.id]]
+        
+        return []
+
+    def tier_has_regions(self, tier):
+        return False
+
+    def region_for_annotation(self, annotation):
+        pass
